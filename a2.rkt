815fb8572ddea81a8c861c56cb9a974de0eb9e92
Hiroki Chen <haobchen@iu.edu>
2023-09-04 12:22:50 -0400

    Finish part of HW2

--- a2.rkt
+++ a2.rkt
@@ -1,6 +1,4 @@
 #lang racket
- (require racket/trace)
-
 (define list-ref
   (lambda (ls n)
     (letrec
@@ -10,7 +8,6 @@
                     (else (cdr (nth-cdr (sub1 n)))))
                   )])
       (car (nth-cdr n)))))
-
 
 (define union
   (λ (lhs rhs)
@@ -39,13 +36,91 @@
 
 (define lambda-exp?
   (λ (E)
-    ))
+    (letrec
+        ([p
+          (λ (e)
+            (match e
+              [`,y #:when (symbol? y) #t]
+              [`(lambda (,x) ,body) (p body)]
+              [`(,rator, rand) (and (p rator) (p rand))]
+              [else #f]))])
+      (p E))))
 
-(define parse/match
+(define var-occurs?
+  (λ (var e)
+    (match e
+      [`,y #:when (symbol? y) (eqv? var y)]
+      [`(lambda (,x) ,body) (var-occurs? var body)]
+      [`(,rator ,rand) (or (var-occurs? var rator) (var-occurs? var rand))])))
+
+(define vars
   (λ (e)
     (match e
-      (`,y #:when (symbol? y) y)
-      (`(λ (,x) ,body) #:when (symbol? x)
-       `(lambda (,x) ,(parse/match body)))
+      [`,y #:when (symbol? y) (cons y null)]
+      [`(lambda (,x) ,body) (vars body)]
+      [`(,rator ,rand) (append (vars rator) (vars rand))])))
+
+(define unique-vars
+  (λ (e)
+    (match e
+      [`,y #:when (symbol? y) (cons y null)]
+      [`(lambda (,x) ,body) (union (cons x null) (unique-vars body))]
+      [`(,rator ,rand) (union (unique-vars rator) (unique-vars rand))])))
+
+(define var-occurs-free?
+  (λ (x e)
+    (match e
+      [`,y #:when (symbol? y) (eqv? x y)]
+      [`(lambda (,y) ,body) (and (not (eqv? x y)) (var-occurs-free? x body))]
+      [`(,rator ,rand) (or (var-occurs-free? x rator) (var-occurs-free? x rand))])))
+
+(define var-occurs-bound?
+  (λ (x e)
+    (match e
+      [`,y #:when (symbol? y) #f]
+      [`(lambda (,y) ,body) (and (eqv? x y) (var-occurs-free? x body))]
+      [`(,rator ,rand) (or (var-occurs-bound? x rator) (var-occurs-bound? x rand))])))
+
+(define unique-free-vars
+  (λ (e)
+    (match e
+      [`,y #:when (symbol? y) (cons y null)]
+      [`(lambda (,x) ,body) (remv x (unique-free-vars body))]
+      [`(,rator ,rand) (union (unique-free-vars rator) (unique-free-vars rand))])))
+
+(define unique-bound-vars
+  (λ (e)
+    (match e
+      [`,y #:when (symbol? y) (cons y null)]
+      [`(lambda (,x) ,body) (filter (λ (elem) (eqv? x elem)) (unique-free-vars body))]
+      [`(,rator ,rand) (union (unique-bound-vars rator) (unique-bound-vars rand))])))
+(unique-bound-vars '((lambda (x) ((x y) e)) (lambda (c) (x (lambda (x) (x (e c)))))))
+
+(define index
+  (λ (var ls)
+    (match ls
+      ('() (error "Not found"))
+      (`(,a . ,_) #:when (eqv? a var)
+       0)
+      (`(,_ . ,d)
+       (add1 (index var d))))))
+
+(define lex
+  (λ (e ls)
+    (match e
+      (`,y #:when (symbol? y)
+           (index y ls))
+      (`(lambda (,x) ,body) #:when (symbol? x)
+       `(lambda ,(lex body (cons x ls))))
       (`(,rator ,rand)
-       `(,(parse/match rator) ,(parse/match rand))))))
+       `(,(lex rator ls) ,(lex rand ls))))))
+
+(lex '(lambda (x) x) '(x, y))
+
+(define e1=e2?
+  (λ (e1 e2)
+    (match `(,e1 ,e2)
+      [`(,y1, y2) #:when (eqv? y1 y2) #t]
+      [`((lambda (,x1) ,body1) (lambda (,x2) ,body2))
+        (e1=e2? (lex body1 (unique-bound-vars e1)) (lex body2 (unique-bound-vars e2)))]
+      [`((,rator1 ,rand1) (,rator2 ,rand2)) (and (e1=e2? rator1 rator2) (e1=e2? rand1 rand2))])))
