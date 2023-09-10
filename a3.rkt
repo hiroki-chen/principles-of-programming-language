4009469da4ca35eff26e12b4e69049799b4777d5
Hiroki Chen <haobchen@iu.edu>
2023-09-10 15:34:01 -0400

    Update HW3

--- a3.rkt
+++ a3.rkt
@@ -27,12 +27,21 @@
     (λ (y)
       (cond
         ((eqv? y x) arg)
-        (else(env y))))))
+        (else (env y))))))
 
 ; the support for `set!` and `begin2`.
-;; TODO!!!
+; TODO!!!
 (define value-of-ext
-  (λ (e env) e))
+  (λ (e env)
+    (match e
+      ; evaluation of `begin2` only accepts the last result,
+      ; and ignoring arg1 although it is evaluated.
+      [`(begin2 ,arg1 ,arg2) (begin
+                               (value-of arg1 env)
+                               (value-of arg2 env))]
+      [`(set! ,key ,val) #:when (symbol? key)
+                         (set-box! (env key) (value-of val env))]
+     )))
 
 (require racket/trace)
 ; representation-dependent version
@@ -42,25 +51,27 @@
       ; literals
       [`,b #:when (boolean? b) b]
       [`,n #:when (number? n) n]
-      [`(if (zero? ,rand) ,b1 ,b2) (cond
-                          ((zero? (value-of rand env)) (value-of b1 env))
+      [`(if ,c ,b1 ,b2) (cond
+                          ((value-of c env) (value-of b1 env))
                           (else (value-of b2 env)))]
       ; value binding
       [`(let ([,var ,e1]) ,e2) #:when (symbol? var)
-                                   (value-of e2 (extend-env var (value-of e1 env) env)) ]
+                                   (value-of e2 (extend-env var (box (value-of e1 env)) env)) ]
       ; simple functions
+      [`(zero? ,rand) (zero? (value-of rand env))]
       [`(sub1 ,rand) (sub1 (value-of rand env))]
       [`(* ,rand1 ,rand2) (* (value-of rand1 env) (value-of rand2 env))]
       [`(lambda (,x) ,body)
        #:when (symbol? x)
        (λ (arg)
          ; We also need to extend the environment
-         (value-of body
-                 (λ (y)
+         (let ([boxed (box arg)])
+           (value-of body
+                  (λ (y)
                    (cond
-                     ((eqv? y x) arg)
-                     (else (env y))))))]
-      [`,y #:when (symbol? y) (env y)]
+                     ((eqv? y x) boxed)
+                     (else (env y)))))))]
+      [`,y #:when (symbol? y) (unbox (env y))]
       ; application
       [`(,rator ,rand) ((value-of rator env) (value-of rand env))]
       [_ (value-of-ext e env)]
@@ -82,12 +93,12 @@
 
 (define make-clos-fn
   (λ (x body env)
-   `(make-clos-fn ,x ,body ,env)))
+    `(mk-clos (lambda (,x) ,body) ,env)))
 
 (define apply-clos-fn
   (λ (f arg)
     (match f
-    [`(make-clos-fn ,x ,body ,env) (value-of-fn body (extend-env-fn x arg env))])))
+    [`(mk-clos (lambda (,x) ,body) ,env) (value-of-fn body (extend-env-fn x arg env))])))
 
 ; representation-independent version
 (define value-of-fn
@@ -96,18 +107,19 @@
       ; literals
       [`,b #:when (boolean? b) b]
       [`,n #:when (number? n) n]
-      [`(if (,c ,rand) ,b1 ,b2) (cond
-                          ((c (value-of rand env)) (value-of b1 env))
-                          (else (value-of b2 env)))]
+      [`(if ,c ,b1 ,b2) (cond
+                          ((value-of-fn c env) (value-of-fn b1 env))
+                          (else (value-of-fn b2 env)))]
       ; value binding
       [`(let ([,var ,e1]) ,e2) #:when (symbol? var)
-                                   (value-of e2 (extend-env var (value-of e1 env) env)) ]
+                                   (value-of-fn e2 (extend-env-fn var (value-of-fn e1 env) env)) ]
       ; simple functions
-      [`(sub1 ,rand) (sub1 (value-of rand env))]
-      [`(* ,rand1 ,rand2) (* (value-of rand1 env) (value-of rand2 env))]
+      [`(zero? ,rand) (zero? (value-of-fn rand env))]
+      [`(sub1 ,rand) (sub1 (value-of-fn rand env))]
+      [`(* ,rand1 ,rand2) (* (value-of-fn rand1 env) (value-of-fn rand2 env))]
       [`,y #:when (symbol? y) (apply-env-fn env y)]
       ; make closure and apply it
       [`(lambda (,x) ,body) #:when (symbol? x) (make-clos-fn x body env)]
       ; application
-      [`(,rator ,rand) ((value-of rator env) (value-of rand env))]
+      [`(,rator ,rand) (apply-clos-fn (value-of-fn rator env) (value-of-fn rand env))]
       )))
